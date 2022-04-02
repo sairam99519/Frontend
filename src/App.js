84ec49e52ba18aa80df4f485dc70f52b7ba4949a
root <root@ASLi3101.achala.local>
2022-04-02 17:46:55 +0200

    bug fixed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Future Bank!!!!</p>
+        <p>Welcome to Future World!!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
